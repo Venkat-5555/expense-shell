67d3c32d844cf94fff7ad9681d47b87766edef7b
venkat-5555 <venkatsaikrishna5555@gmail.com>
2025-03-25 18:49:16 +0530

    added partial backend

---
+++ backend.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/expense"
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%s)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.sh"
+mkdir -p $LOG_FOLDER
+
+USER_ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+check_root(){
+    if [ $USER_ID -ne 0 ]
+    then
+        echo -e "$R please run the user with root credentials $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.......$R FAILED $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is........$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
+check_root
+
+dnf module disable nodejs -y
+validate $? "Disable default nodejs"
+
+dnf module enable nodejs:20 -y
+validate $? "Enable nodejs:20"
+
+dnf install nodejs -y
+validate $? "nodejs install"
+
+useradd expense
+validate $? "adding expense user"
+
+
